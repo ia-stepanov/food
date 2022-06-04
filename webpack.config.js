d1157450bd9228f9f01b8878d2056a13329f6417
Igor Stepanov <igorstepanov@hotmail.com>
2022-06-04 17:29:01 +0300

    build(Food): транспилировать код под старый формат

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,32 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/js'
+    path: __dirname + '/js',
   },
   watch: true,
 
-  devtool: "source-map",
+  devtool: 'source-map',
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
